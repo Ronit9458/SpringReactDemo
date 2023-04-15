63ac528e731982dba1632b1904b76d3d8f9bccdf
Ronit <mailto:ronit.jaiswal@opalina.in>
2023-04-15 13:51:14 +0530

    api

---
+++ cm_ui_api/src/main/java/com/master/contact/api/CmUiApiApplication.java
@@ -0,0 +1,13 @@
+package com.master.contact.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmUiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmUiApiApplication.class, args);
+	}
+
+}
---
+++ cm_ui_api/src/test/java/com/master/contact/api/CmUiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.contact.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmUiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

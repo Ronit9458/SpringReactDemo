3f0bf28d79a7b349e0f5d67d70de31daab45bac9
Ronit <mailto:ronit.jaiswal@opalina.in>
2023-04-16 15:06:12 +0530

    changes in controllwe

--- cm_ui_api/src/main/java/com/master/contact/api/controllers/UserController.java
+++ cm_ui_api/src/main/java/com/master/contact/api/controllers/UserController.java
@@ -1,10 +1,31 @@
 package com.master.contact.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.master.contact.api.model.UserModel;
+import com.master.contact.api.services.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/create")
+	public ResponseEntity<UserModel> createUser(@RequestBody UserModel model)
+	{
+		UserModel userModel = this.userService.createUser(model);
+		ResponseEntity<UserModel> response = new ResponseEntity<>(userModel,HttpStatus.CREATED);
+		return response;
+		
+	}
 
 }

5e934bfba84601d2a749ae4a76566c11dc9320ce
Ronit <mailto:ronit.jaiswal@opalina.in>
2023-04-17 18:36:25 +0530

    adding validation jar

--- cm_ui_api/src/main/java/com/master/contact/api/exception/GlobalExceptionHandler.java
+++ cm_ui_api/src/main/java/com/master/contact/api/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.master.contact.api.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,4 +23,19 @@ public class GlobalExceptionHandler {
 	APIResponse aPIResponse = new APIResponse(message,false);
 	return new ResponseEntity<APIResponse>(aPIResponse,HttpStatus.NOT_FOUND);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex)
+	{
+		Map<String, String> resp=new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error)-> {
+		String field = ((FieldError)error).getField();	
+		String message = error.getDefaultMessage();
+		resp.put(field, message);
+		});
+		
+		return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
+		
+	}
 }
+

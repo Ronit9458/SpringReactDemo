6b3cd629ee194a82f852b51e1398966ce67774ae
sharik <sharikkhan9012@gmail.com>
2023-04-18 06:36:57 +0530

    create User, Contact Entity and Model with Lombok

--- cm_ui_api/src/main/java/com/master/contact/api/entities/User.java
+++ cm_ui_api/src/main/java/com/master/contact/api/entities/User.java
@@ -1,5 +1,65 @@
 package com.master.contact.api.entities;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "USR")
 public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USR_GEN")
+	@SequenceGenerator(name = "SEQ_USR_GEN", sequenceName = "SEQ_USR", allocationSize = 1)
+	@Column(name = "ID")
+	Long id;
+	
+	@Column(name = "REF_ID")
+	String refId;
+	
+	@Column(name = "PSWD")
+	String pswd;
+	
+	@Column(name = "EMAIL")
+	String email;
+	
+	@Column(name = "USR_ROLE")
+	String userRole;
+	
+	@Column(name = "CNTCT_NUMBER")
+	String cntctNumber;
+	
+	@Column(name = "IS_ACTIVE")
+	String isActive;
+	
+	@Column(name = "ADDRESS")
+	String address;
+	
+	@Column(name = "CRT_BY")
+	String crtBy = "SYSTEM";
+	
+	@Column(name = "CRT_TS")
+	Date crtTs = new Date();
+	
+	@Column(name = "UPD_BY")
+	String updBy;
+	
+	@Column(name = "UPD_TS")
+	Date updTs = new Date();
+	
+	
+	
 
 }
--- cm_ui_api/src/main/java/com/master/contact/api/model/ContactModel.java
+++ cm_ui_api/src/main/java/com/master/contact/api/model/ContactModel.java
@@ -1,5 +1,28 @@
 package com.master.contact.api.model;
 
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
 public class ContactModel {
+	
+	Long id;
+	Long userId;
+	String name;
+	String homeNo;
+	String email;
+	String address;
+	String image;
+	String descr;
+	Date crtTs;
+	String crtBy;
+	Date updTs;
+	String updBy;
+	Date delTs;
+	String delBy;
 
 }
